f5a593f648ff9f03851a52b1bd2a32c0df685e30
Taozand001 <taofeekamodu01@gmail.com>
2025-09-01 15:24:26 +0100

    Update HackBio stage zero.sh

--- HackBio stage zero.sh
+++ HackBio stage zero.sh
@@ -1,87 +1,69 @@
 Project 1: BASh Basic
 
-# 1. print your name
-echo 'Taofeek'
+#!/bin/bash 
+   
+    #question 1
+echo "Taofeek"
 
-# 2. Create a folder titled your name
-mkdir 'Taofeek'
+      #question 2    making a new directory
+mkdir Taofeek
 
-# 3. Create another new directory titled biocomputing and change to that directory with one line of command
-mkdir 'biocomputing' && cd 'biocomputing'
+      #question 3  make another directory 'biocomputing' and change to it with one command line
+mkdir biocomputing && cd biocomputing
 
-# 4. Download 3 files
-wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+      #question 4   downloading multiple files
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna 
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk 
 wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
-wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
 
-# 5. move '.fna' file to the folder named Taofeek
+       #question 5    move the .fna file to folder named Taofeek
 mv wildtype.fna Taofeek/
 
-# 6. delete the duplicate '.gbk' file
-rm wildtype.gbk.1 
+       #question 6   delete the duplicate file
+rm wildtype.gbk.1
 
-# 7. confirm if the .fna file is mutant or wild type (tatatata vs tata)
- grep 'tatatata' wildtype.fna 
+       #question 7    confirming if its a mutant or a wild type
+grep 'tatatata' ../Taofeek/wildtype.fna
 
-# 8. If mutant, print all matching lines into a new file
-grep 'tatatata' wildtype.fna > mutant_line.txt
+       #question 8   printing matching lines into a new file
+grep 'tatatata' wildtype.fna > mutant_files.txt
 
-# 9.  Count number of lines (excluding header) in the .gbk file
-tail -n 5749 wildtype.gbk
+       #question 9  counting the number of lines excluding header in the gbk file
+sed '1d' wildtype.gbk | wc -l
 
-# 10.  Print the sequence length of the .gbk file. (Use the LOCUS tag in the first line)
- head -n 1 wildtype.gbk | awk '{print $3}'
+       #question 10  printing the sequence length of the gbk file
+grep 'LOCUS' wildtype.gbk
 
-# 11.  Print the source organism of the .gbk file. (Use the SOURCE tag in the first line)
-head -n 6 wildtype.gbk | awk '{print $2}'
+       #question 11   printing the source organism of the gbk file
+grep 'SOURCE' wildtype.gbk
 
-# 12. List all the gene names of the .gbk file. Hint {grep '/gene='}
-grep 'gene' wildtype.gbk
+       #question 12   listing the gene names of the gbk file
+grep '/gene' wildtype.gbk
 
-# 13. Clear your terminal space and print all commands used today
-clear 
-history
+      #question 13   clearing terminal and printing the history of commands executed
+clear && history
 
-# 14. List files in the two folder
-  ls biocomputing/ Taofeek/
+#question 14       list the files in the two folders
+ls biocomputing/ Taofeek/
 
 Project 2: Installing Bioinformatics Software on the Terminal
 
-# 1. Activate your base conda environment
- conda activate base
+#!/bin/bash
 
-# 2. Create a conda environment named funtools
-conda create -n "funtools"
+#question 1         activate base conda
+conda activate base
 
-# 3. Activate the funtools environment
+ #question 2        create conda environment named 'funtools'
+conda create -n 'funtools'
+
+ #question 3        activate 'funtools'
 conda activate funtools
 
-# 4. Install Figlet using conda or apt-get
- sudo apt-get install figlet -y
+ #question 4       Install 'Figlet' using conda 
+conda install figlet -y
 
-# 5.  Run figlet <your name>
-figlet Taofeek
-
-# 6. Install bwa through the bioconda channel 
-conda install -c bioconda bwa
-
-# 7. Install blast through the bioconda channel
-conda install -c bioconda blast
-
-# 8.  Install samtools through the bioconda channel
-conda install -c bioconda samtools
-
-# 9.  Install bedtools through the bioconda channel
-conda install -c bioconda bedtools 
-
-# 10. Install spades.py through the bioconda channel
-conda install -c bioconda spades.py
-
-# 11.  Install bcftools through the bioconda channe
-conda install -c bioconda bcftools
-
-# 12. Install fastp through the bioconda channel
-conda install -c bioconda fastp
-
-# 13. Install multiqc through the bioconda channel
- conda install -c bioconda multiqc
+ #question 5       run figlet 'your name'
+figlet 'Taofeek'
+ 
+ #question 6-13 install all packages through bioconda channel
+conda install -c bioconda bwa blast samtools bedtools spades.spy bcftools fastp multiqc
